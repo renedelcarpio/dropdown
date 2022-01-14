29a4e2dfdb5e3fbfdab5334f7086efd5eb675ac1
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-14 17:42:17 -0400

    create dropdoun menu component

---
+++ src/components/DropdownMenu/DropdownMenu.js
@@ -0,0 +1,42 @@
+import './DropdownMenu.scss';
+
+import DropdownItem from '../DropdownItem/DropdownItem/DropdownItem';
+import React from 'react';
+import { useState } from 'react/cjs/react.development';
+
+const DropdownMenu = ({ setSelectedOption, selectedOption }) => {
+	const options = ['id', 'reference_name', 'nit', 'name'];
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleToggle = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const onOptionSelected = ({ target }) => {
+		setSelectedOption(target.innerHTML);
+		setIsOpen(false);
+	};
+
+	return (
+		<>
+			<div
+				className={isOpen ? 'dropdown__menu--open' : 'dropdown__menu'}
+				onClick={handleToggle}
+			>
+				Dropdown Menu
+			</div>
+			{isOpen && (
+				<div className='dropdown__items'>
+					{options.map((option, index) => (
+						<DropdownItem onClick={onOptionSelected} key={`${option}${index}`}>
+							{option}
+						</DropdownItem>
+					))}
+				</div>
+			)}
+		</>
+	);
+};
+
+export default DropdownMenu;
