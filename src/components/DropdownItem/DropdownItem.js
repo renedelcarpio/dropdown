f6941f99b0bb6eb5bfdc2f5b02bd71b4ffde6969
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-14 20:43:33 -0400

    document DropdownItem component

--- src/components/DropdownItem/DropdownItem.js
+++ src/components/DropdownItem/DropdownItem.js
@@ -1,5 +1,11 @@
 import './DropdownItem.scss';
 
+/**
+ * @param {String} children This is the string that the tag will show
+ * @param {function}	onClick	This function is called in father component.
+ * @returns Returns the string assigned to children
+ */
+
 const DropdownItem = ({ children, onClick }) => {
 	return (
 		<p className='item__container' value={children} onClick={onClick}>
